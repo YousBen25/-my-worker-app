84920260f95dfaa185b1142df8810445db8d2576
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2020-12-01 07:58:17 +0800

    added tags form to worker_profile edit view

--- app/controllers/worker_profile_tags_controller.rb
+++ app/controllers/worker_profile_tags_controller.rb
@@ -1,7 +1,17 @@
 class WorkerProfileTagsController < ApplicationController
   def create
+    @worker_profile = WorkerProfile.find(params[:worker_profile_id])
+    @tags = Tag.find(params[:worker_profile_tag][:tag])
+    @tags.each do |tag|
+      @worker_profile_tag = WorkerProfileTag.new
+      @worker_profile_tag.worker_profile = @worker_profile
+      @worker_profile_tag.tag = tag
+      @worker_profile_tag.save!
+    end
   end
 
   def destroy
+    @worker_profile_tag = WorkerProfileTag.find(params[:id])
+    @worker_profile_tag.destroy
   end
 end
