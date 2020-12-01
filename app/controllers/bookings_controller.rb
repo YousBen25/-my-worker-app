ab7f980bd3ea7e14dac21993782f4b8c9c9366bb
Rosita Evillya <rosita.evillya@googlemail.com>
2020-12-01 11:02:47 +0700

    implement booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,13 @@
 class BookingsController < ApplicationController
+
+  # before_action :set_booking
   def new
+    @worker_profile = Booking.find(params[:worker_profile_id])
+    @booking = Booking.new
   end
 
   def create
+   
   end
 
   def edit
@@ -12,5 +17,13 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def booking_params
+    params.require(:booking).permit(:description, :date, :duration, :price, :confirmation, )
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:worker_profile_id])
   end
 end
