3a775c3bb750a8816983aa953636b7c53684f40f
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2020-12-01 16:14:50 +0800

    controller actions ok

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,8 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:edit, :update, :destroy]
   def new
+    @worker_profile_tag = WorkerProfileTag.find()
+    @booking = Booking.new
   end
 
   def create
@@ -13,4 +16,26 @@ class BookingsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:description, :date, :duration)
+  end
 end
+
+# t.bigint "user_id", null: false
+    # t.text "description"
+    # t.bigint "worker_profile_tag_id", null: false
+    # t.boolean "confirmation", default: false
+    # t.datetime "date"
+    # t.integer "duration"
+    # t.integer "price"
+    # t.datetime "created_at", precision: 6, null: false
+    # t.datetime "updated_at", precision: 6, null: false
+    # t.index ["user_id"], name: "index_bookings_on_user_id"
+    # t.index ["worker_profile_tag_id"], name: "index_bookings_on_worker_profile_tag_id"
