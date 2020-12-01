f63fc464d5e72ccb9cf44d6718cd2501ce1df2a6
Nate Gunawan <nategunawan@gmail.com>
2020-12-01 16:45:37 +0800

    good shit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :worker_profiles do
-    resources :bookings, only: [:new, :create]
     resources :worker_profile_tags, only: [:create]
     resources :availabilities, only: [:create]
   end
@@ -16,7 +15,7 @@ Rails.application.routes.draw do
   resources :bookings, only: [:show] do
     resources :reviews, only: [:create]
   end
-
+  resources :bookings, only: [:new, :create]
   resources :worker_profile_tags, only: [:destroy]
   resources :availabilities, only: [:destroy]
 
