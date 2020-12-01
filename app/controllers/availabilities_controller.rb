78b2de49961bb981ef9e5f24c4d2fb026e4a9401
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2020-12-01 12:15:54 +0800

    added params to DateTime.new

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -3,11 +3,25 @@ class AvailabilitiesController < ApplicationController
     @worker_profile = WorkerProfile.find(params[:worker_profile_id])
     @availability = Availability.new(availability_params)
     @availability.worker_profile = @worker_profile
+    @availability.from = DateTime.new(
+                                      params[:availability]["from(1i)"].to_i,
+                                      params[:availability]["from(2i)"].to_i,
+                                      params[:availability]["from(3i)"].to_i,
+                                      params[:availability]["from(4i)"].to_i,
+                                      params[:availability]["from(5i)"].to_i
+                                      )
+    @availability.to = DateTime.new(
+                                      params[:availability]["to(1i)"].to_i,
+                                      params[:availability]["to(2i)"].to_i,
+                                      params[:availability]["to(3i)"].to_i,
+                                      params[:availability]["to(4i)"].to_i,
+                                      params[:availability]["to(5i)"].to_i
+                                      )
     @availability.save
     redirect_to edit_worker_profile_path(@worker_profile)
   end
 
   def availability_params
-    params.require(:availability).permit(:day, :from, :to)
+    params.require(:availability).permit(:day)
   end
 end
