f9e09e9f870cea231e9dc33e4974236522a89148
Rosita Evillya <rosita.evillya@googlemail.com>
2020-12-01 17:17:28 +0700

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,51 +6,51 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# require 'faker'
+require 'faker'
 
-# Tag.create!(name: "Plumbing")
-# Tag.create!(name: "Landscaping")
-# Tag.create!(name: "Driving")
-# Tag.create!(name: "Housekeeping")
-# Tag.create!(name: "Cooking")
+Tag.create!(name: "Plumbing")
+Tag.create!(name: "Landscaping")
+Tag.create!(name: "Driving")
+Tag.create!(name: "Housekeeping")
+Tag.create!(name: "Cooking")
 
 
-# puts "start seeding"
-# 50.times do
-#   user = User.create(first_name: Faker::Name.first_name,
-#     last_name: Faker::Name.last_name,
-#     email: Faker::Internet.email,
-#     password: "password")
-#   if rand(0..1) == 1
-#     worker_profile = WorkerProfile.create!(user: user, bio: "some bio")
-#     rand(1..3).times do
-#       WorkerProfileTag.create!(rate: rand(10..50), worker_profile: worker_profile, tag: Tag.all.sample)
-#     end
-#   end
-#   puts "seeding user"
-# end
+puts "start seeding"
+50.times do
+  user = User.create(first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "password")
+  if rand(0..1) == 1
+    worker_profile = WorkerProfile.create!(user: user, bio: "some bio")
+    rand(1..3).times do
+      WorkerProfileTag.create!(rate: rand(10..50), worker_profile: worker_profile, tag: Tag.all.sample)
+    end
+  end
+  puts "seeding user"
+end
 
-# puts "start seeding bookings"
+puts "start seeding bookings"
 
-# 50.times do
-#   booking = Booking.create!(
-#     user: User.all.sample,
-#     worker_profile_tag: WorkerProfileTag.all.sample,
-#     description: Faker::Lorem.sentence,
-#     confirmation: [true, false].sample,
-#     date: Date.today + rand(0..30),
-#     duration: rand(1..5),
-#     price: 100
-#     )
-#   review = Review.create!(
-#     score: rand(1..5),
-#     description: Faker::Lorem.sentence,
-#     booking: booking,
-#     user: booking.user,
-#     )
+50.times do
+  booking = Booking.create!(
+    user: User.all.sample,
+    worker_profile_tag: WorkerProfileTag.all.sample,
+    description: Faker::Lorem.sentence,
+    confirmation: [true, false].sample,
+    date: Date.today + rand(0..30),
+    duration: rand(1..5),
+    price: 100
+    )
+  review = Review.create!(
+    score: rand(1..5),
+    description: Faker::Lorem.sentence,
+    booking: booking,
+    user: booking.user,
+    )
 
-#   puts "seeding bookings and reviews"
-# end
+  puts "seeding bookings and reviews"
+end
 
 def pick_rand_time
   from = rand(8..20)
