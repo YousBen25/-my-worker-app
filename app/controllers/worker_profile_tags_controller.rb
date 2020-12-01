4d0b79b85985393f33c05282a1eab43a4ca6ebf5
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2020-12-01 10:49:00 +0800

    added the rates to the view

--- app/controllers/worker_profile_tags_controller.rb
+++ app/controllers/worker_profile_tags_controller.rb
@@ -1,13 +1,13 @@
 class WorkerProfileTagsController < ApplicationController
   def create
     @worker_profile = WorkerProfile.find(params[:worker_profile_id])
-    @tags = Tag.find(params[:worker_profile_tag][:tag])
-    @tags.each do |tag|
-      @worker_profile_tag = WorkerProfileTag.new
-      @worker_profile_tag.worker_profile = @worker_profile
-      @worker_profile_tag.tag = tag
-      @worker_profile_tag.save!
-    end
+    tag = Tag.find(params[:worker_profile_tag][:tag])
+    @worker_profile_tag = WorkerProfileTag.new
+    @worker_profile_tag.worker_profile = @worker_profile
+    @worker_profile_tag.tag = tag
+    @worker_profile_tag.save
+    redirect_to edit_worker_profile_path(@worker_profile)
+
   end
 
   def destroy
