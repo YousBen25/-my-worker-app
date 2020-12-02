0bd22709f92fb0eb37b674e08d005442b6b7c7a3
Ramadhani Putra <rpputra@kth.se>
2020-12-02 14:33:21 +0800

    adding sandbox and experimental working map on worker index page

--- app/controllers/worker_profiles_controller.rb
+++ app/controllers/worker_profiles_controller.rb
@@ -13,7 +13,6 @@ class WorkerProfilesController < ApplicationController
   end
 
   def show
-    raise
     @user = current_user
     @booking = Booking.new
     @working_hash = @worker_profile.calculate_availabilities
