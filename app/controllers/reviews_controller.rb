5b6cbbc5cdf8d2bfd0c3dd62eef82c28c0aab71d
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2020-12-03 17:15:33 +0800

    done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,12 @@
 class ReviewsController < ApplicationController
   def create
   @review = Review.new(review_params)
-  @worker_profile = WorkerProfile.find(params[:worker_profile_id])
-  @review.worker_profile = @worker_profile
-  @review.save
+  @booking = Booking.find(params[:booking_id])
+  @review.booking = @booking
+  @review.user = current_user
   authorize @review
   if @review.save
-    redirect_to worker_profile_path(@worker_profile)
+    redirect_to booking_path(@booking)
   else
     render :new
   end
