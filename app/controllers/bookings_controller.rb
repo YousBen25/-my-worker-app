92b940b3dadfe2dfe406ebb613ad7dd008d7d896
Nate Gunawan <nategunawan@gmail.com>
2020-12-03 15:00:41 +0800

    quick

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,12 +10,12 @@ class BookingsController < ApplicationController
     # date = date.map(&:to_i)
     # time = booking_vars["from"].to_i
     # workertag = WorkerProfileTag.find(params[:booking][:worker_profile_tag_id]) if params[:booking][:worker_profile_tag_id.present?
-    
+
     @worker_profile= WorkerProfile.find(params[:worker_profile_id])
     @booking = Booking.new(booking_params)
     @booking.date = generate_date
     @booking.user = current_user
-    
+
     # @booking = Booking.new(
     #   description: booking_vars["description"],
     #   worker_profile_tag_id: booking_vars["worker_profile_tag_id"],
@@ -70,7 +70,7 @@ end
       # date: DateTime.new(date[0], date[1], date[2], time),
       # duration: booking_vars["duration"],
       # price: price,
-    @booking.date = generate_date 
+    @booking.date = generate_date
     if @booking.update(booking_params)
       redirect_to booking_path(@booking)
     else
@@ -112,6 +112,7 @@ end
       address: booking_vars["address"],
       state: "pending"
     }
+  end
 
   def generate_date
     return nil if params[:booking]["date"].blank? ||  params[:booking]["from"].blank?
@@ -131,14 +132,4 @@ end
   end
 end
 
-# t.bigint "user_id", null: false
-    # t.text "description"
-    # t.bigint "worker_profile_tag_id", null: false
-    # t.boolean "confirmation", default: false
-    # t.datetime "date"
-    # t.integer "duration"
-    # t.integer "price"
-    # t.datetime "created_at", precision: 6, null: false
-    # t.datetime "updated_at", precision: 6, null: false
-    # t.index ["user_id"], name: "index_bookings_on_user_id"
-    # t.index ["worker_profile_tag_id"], name: "index_bookings_on_worker_profile_tag_id"
+end
