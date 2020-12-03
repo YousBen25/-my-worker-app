e3af73bd921a199c3a33017e7da68732ef80beaf
Nate Gunawan <nategunawan@gmail.com>
2020-12-03 15:22:06 +0800

    edits to stripe

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -12,7 +12,7 @@ class BookingsController < ApplicationController
     # workertag = WorkerProfileTag.find(params[:booking][:worker_profile_tag_id]) if params[:booking][:worker_profile_tag_id.present?
 
     @worker_profile= WorkerProfile.find(params[:worker_profile_id])
-    @booking = Booking.new(booking_params)
+    @booking = Booking.new(custom_booking_params)
     @booking.date = generate_date
     @booking.user = current_user
 
@@ -53,8 +53,8 @@ class BookingsController < ApplicationController
         lat: @booking.latitude,
         lng: @booking.longitude
       }]
+    end
   end
-end
 
   def edit
     @worker_profile = @booking.worker_profile_tag.worker_profile
@@ -110,6 +110,7 @@ end
       date: DateTime.new(date[0], date[1], date[2], time),
       duration: booking_vars["duration"],
       address: booking_vars["address"],
+      worker_profile_tag_id: booking_vars["worker_profile_tag_id"],
       state: "pending"
     }
   end
@@ -130,6 +131,6 @@ end
   def booking_params
     params.require(:booking).permit(:worker_profile_tag_id, :description, :duration, :address)
   end
-end
 
 end
+
